57eb98226b22b37acffa5b867f3f440c00d29ddb
Rosdyana Kusuma <rosdyana.kusuma@gmail.com>
2025-01-03 17:23:20 +0800

    [fix] update gitignore and add code for hw1

--- student_assignment.py
+++ student_assignment.py
@@ -4,37 +4,49 @@ import traceback
 from model_configurations import get_model_configuration
 
 from langchain_openai import AzureChatOpenAI
-from langchain_core.messages import HumanMessage
+from langchain_core.messages import HumanMessage, SystemMessage
 
-gpt_chat_version = 'gpt-4o'
+gpt_chat_version = "gpt-4o"
 gpt_config = get_model_configuration(gpt_chat_version)
 
+
 def generate_hw01(question):
-    pass
-    
+    prompt = "Please format the result as JSON with Result: { date, name }"
+    answer = demo(question, prompt)
+
+    # Strip the markdown code block and parse the JSON content
+    json_content = answer.content.strip("```json\n").strip("```")
+    parsed_answer = json.loads(json_content)
+    print(parsed_answer)
+
+
 def generate_hw02(question):
     pass
-    
+
+
 def generate_hw03(question2, question3):
     pass
-    
+
+
 def generate_hw04(question):
     pass
-    
-def demo(question):
+
+
+def demo(question, prompt):
     llm = AzureChatOpenAI(
-            model=gpt_config['model_name'],
-            deployment_name=gpt_config['deployment_name'],
-            openai_api_key=gpt_config['api_key'],
-            openai_api_version=gpt_config['api_version'],
-            azure_endpoint=gpt_config['api_base'],
-            temperature=gpt_config['temperature']
+        model=gpt_config["model_name"],
+        deployment_name=gpt_config["deployment_name"],
+        openai_api_key=gpt_config["api_key"],
+        openai_api_version=gpt_config["api_version"],
+        azure_endpoint=gpt_config["api_base"],
+        temperature=gpt_config["temperature"],
     )
-    message = HumanMessage(
-            content=[
-                {"type": "text", "text": question},
-            ]
+    system_message = SystemMessage(content=prompt)
+    human_message = HumanMessage(
+        content=[
+            {"type": "text", "text": question},
+        ]
     )
-    response = llm.invoke([message])
-    
+    response = llm.invoke([system_message, human_message])
+
     return response
